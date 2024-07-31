669dbae8e00998230a709bcb64ad08ee034ea2d6
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-07-31 13:31:41 -0400

    ahaha

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,18 +1,22 @@
 // Recursion Problem Set: Coding Exercises
 
 // Calculate and return the total sum using recursive approach
-const sum = (arr) => {}
+const sum = (arr = [], sumOfArray = 0, index = 0) => {
+  if (arr.length === index) return sumOfArray
+  return sum(arr, sumOfArray + arr[index], index + 1)
+
+}
 
 // Reverse string using recursive approach
-const reverse = (str) => {};
+const reverse = (str) => { };
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => {};
+const fibIter = (n) => { };
 
 // Recursive fibonacci
-const fibRec = (n) => {};
+const fibRec = (n) => { };
 
 // Return the index of target in arr, or -1 if not found using recursion 
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => { };
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
