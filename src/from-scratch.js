5a71b58d9e608db7225a61c0b1b36f6ffd316d1c
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-08-01 10:13:38 -0400

    updated

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -8,10 +8,34 @@ const sum = (arr = [], sumOfArray = 0, index = 0) => {
 }
 
 // Reverse string using recursive approach
-const reverse = (str) => { };
+const reverse = (str) => {
+  if (str.length === 1) return str
+  return reverse(str.substring(1)) + str[0]
+};
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => { };
+const fibIter = (n) => {
+  //guard clause for 0 input
+  if (n === 0) return 0
+
+  // get the fib number --> add thhe previous number with the curr number
+  let first = 0
+  let second = 1
+  let fib = first + second
+
+  // start building the sequence iteraively
+  //loop n amount of times to build the nth fib number
+  for (let i = 2; i < n; i++) {
+    // update first
+    first = second
+    //update second
+    second = fib
+    // update fib
+    fib = first + second
+  }
+  return fib
+};
+
 
 // Recursive fibonacci
 const fibRec = (n) => { };
